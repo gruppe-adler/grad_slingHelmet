481b27a48efc6c96a7b702e8593d12b8543f9a4a
DerZade <DerZade@gmail.com>
2018-03-31 17:12:42 +0200

    Added icon to ACE Action

--- addons/GRAD_slingHelmet/ACE_SelfActions.hpp
+++ addons/GRAD_slingHelmet/ACE_SelfActions.hpp
@@ -6,7 +6,7 @@ class GRAD_slingHelmet_sling
     statement = "[_player] call GRAD_slingHelmet_fnc_actionSling";
     showDisabled = 0;
     priority = 2.5;
-    icon = "";
+    icon = "\a3\ui_f\data\gui\rsc\rscdisplayarsenal\headgear_ca.paa";
 };
 class GRAD_slingHelmet_unsling : GRAD_slingHelmet_sling
 {
